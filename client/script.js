020421f5b3a671e486b703e080c313336d51880c
Johnny Kang <junyoung@my.yorku.ca>
2023-01-03 16:36:31 -0500

    Commit

--- client/script.js
+++ client/script.js
@@ -45,7 +45,7 @@ function chatStripe(isAi, value, uniqueId) {
   return `
    <div class="wrapper ${isAi && "ai"}">
     <div class="chat">
-      <div className="profile">
+      <div class="profile">
        <img src="${isAi ? bot : user}" alt="${isAi ? "bot" : "user"}" />
       </div>
       <div class="message" id=${uniqueId}> ${value}</div>
@@ -73,6 +73,31 @@ const handleSubmit = async (e) => {
   const messageDiv = document.getElementById(uniqueId);
 
   loader(messageDiv);
+
+  const response = await fetch("https://localhost:5000", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: data.get("prompt"),
+    }),
+  });
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = "Santa is thinking";
+
+  if (response.ok) {
+    const data = await response.json();
+    const parseData = data.bot.trim();
+    console.log(parseData);
+
+    typeText(messageDiv, parseData);
+  } else {
+    const err = await response.text();
+    messageDiv.innerHTML = "Something went wrong";
+    alert(err);
+  }
 };
 
 form.addEventListener("submit", handleSubmit);
